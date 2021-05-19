d5f1388b5e5e2e2cbf77af0733b6cee9767ce336
Anastasiia <ergardanastasiia@gmail.com>
2021-05-19 12:22:38 +0300

    виконала обчислення

---
+++ Java Script/lesson01_/demo01.js
@@ -0,0 +1,5 @@
+var x = 5;
+var y = 9;
+var z = 568.9;
+var sa = (x + y + z) / 3;
+console.log(sa);
---
+++ Java Script/lesson01_/demo02.js
@@ -0,0 +1,6 @@
+var x = 15;
+var y = 7;
+var w = 2;
+var h = 10;
+var square = (x * y) / (w * h);
+console.log(square);
